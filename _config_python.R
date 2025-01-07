ae65e87a61b111398ab701bea3857aad09db303a
Andrew Heiss <andrew@andrewheiss.com>
2025-01-07 17:15:20 -0500

    Use version-specific python

--- _config_python.R
+++ _config_python.R
@@ -1,4 +1,5 @@
-reticulate::virtualenv_create("datavizsp25")
+reticulate::install_python("3.12.7")
+reticulate::virtualenv_create("datavizsp25", version = "3.12.7")
 reticulate::py_install(c("cleannlp", "spacy"), envname = "datavizsp25")
 reticulate::use_virtualenv("datavizsp25")
 cleanNLP::cnlp_download_spacy("en_core_web_sm")
